fa1cba370073a68afd5eeb1f2f8339f179f0e31b
unknown <jrglomar016@gmail.com>
2021-02-05 18:42:40 +0800

    form added

--- application/views/user/projects.php
+++ application/views/user/projects.php
@@ -35,7 +35,39 @@ The above copyright notice and this permission notice shall be included in all c
         <div class="container-fluid">
           <div class="row">
             <!-- END OF OPENING TAG OF CONTENT -->
-            
+
+            <!-- PROJECTS DATA TABLE -->
+            <div class="col-md-12">
+              <div class="card">
+                  <div class="card-header card-header-info">
+                    <div class="d-flex justify-content-between">
+                      <h3 class="card-title pull-left">Projects</h3>
+                      <button data-toggle="modal" data-target="#newProjectModal" data-dismiss="modal"
+                        type="submit" class="btn btn-success pull-right"><i class="material-icons">add</i> New Project
+                      </button>  
+                    </div>
+                    <p class="card-category"> List of Project Proposal</p>
+                  </div>
+                <div class="card-body">
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead class=" text-sucess">
+                          <th>Title</th>
+                          <th>Implementer</th>
+                          <th>Target Group</th>
+                          <th>Date Start</th>
+                          <th>Status</th>
+                      </thead>
+                      <tbody>
+                        
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+             <!-- END OF PROJECTS DATA TABLE -->
+
             <!-- CLOSING TAG OF CONTENT -->
           </div>
         </div>
@@ -56,9 +88,19 @@ The above copyright notice and this permission notice shall be included in all c
   <!-- FIXED PLUGINS -->
   <?php $this->load->view('includes/core_js_files.php')?>
   
+  <!-- MODALS -->
+  <?php $this->load->view('modals/user/projects_newProjectModal.php')?>
+
+  <!-- END OF MODALS -->
+
 </body>
 
 <!-- FIXED SCRIPTS -->
 <?php $this->load->view('includes/fixed_scripts.php')?>
+
+<script>
+
+
+</script>
 
 </html>
